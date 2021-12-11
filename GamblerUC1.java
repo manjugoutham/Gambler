37bdb41b71ab7759d92454cf867d9dbc725492dd
manjugoutham <gouthamy219@gmail.com>
2021-12-11 18:26:28 +0530

    Welcome GamblerUC

---
+++ GamblerUC1.java
@@ -0,0 +1,20 @@
+package workshop;
+
+import java.util.Scanner;
+
+public class GamblerUC1 
+{
+	
+	public static void main(String[] args) 
+	{
+		 Scanner scan =new Scanner(System.in); 
+	        int bet,stake;
+	        
+	        System.out.print("Enter the stak  : ");  
+	        stake =scan.nextInt(); 
+	        
+	        System.out.print("Enter the bet: ");  
+	        bet=scan.nextInt();
+	        
+	}
+}
